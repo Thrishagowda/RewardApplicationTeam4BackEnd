aff753ef50a8b7bc5226c5ab82f7968bcb09b632
Harsha-Hanumantha <harshavirat1807@gmail.com>
2025-12-26 06:58:56 +0000

    added dependencies

---
+++ rewardapplicationsys/src/main/java/com/tcs/rewardapplicationsys/RewardapplicationsysApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.rewardapplicationsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardapplicationsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardapplicationsysApplication.class, args);
+	}
+
+}
---
+++ rewardapplicationsys/src/test/java/com/tcs/rewardapplicationsys/RewardapplicationsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.rewardapplicationsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardapplicationsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
