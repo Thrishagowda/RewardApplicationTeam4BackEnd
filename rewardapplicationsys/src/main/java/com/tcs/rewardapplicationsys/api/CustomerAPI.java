b892fee2914b198e9913f4ace0f476ac6987e797
Harsha-Hanumantha <harshavirat1807@gmail.com>
2025-12-29 18:40:16 +0000

    added apis and integerated angular

--- rewardapplicationsys/src/main/java/com/tcs/rewardapplicationsys/api/CustomerAPI.java
+++ rewardapplicationsys/src/main/java/com/tcs/rewardapplicationsys/api/CustomerAPI.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerAPI {
     @Autowired
     CustomerService customerService;
--- rewardapplicationsys/src/main/java/com/tcs/rewardapplicationsys/api/RewardCatalogAPI.java
+++ rewardapplicationsys/src/main/java/com/tcs/rewardapplicationsys/api/RewardCatalogAPI.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/rewards")
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RewardCatalogAPI {
     @Autowired
     private RewardService rewardService;
 
-    @GetMapping("/catalog")
+    @GetMapping("/rewards/catalog")
     public ResponseEntity<List<RewardItem>> getFullCatalog() {
         List<RewardItem> catalog = rewardService.getAllRewards();
         return ResponseEntity.ok(catalog);
